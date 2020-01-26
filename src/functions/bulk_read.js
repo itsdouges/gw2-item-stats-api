b52275631c0c262e3bd7b3ccb79e01d6afb74589
Michael Dougall <mdougall@atlassian.com>
2020-01-27 10:04:06 +1100

    enable cors

--- src/functions/bulk_read.js
+++ src/functions/bulk_read.js
@@ -1,12 +1,25 @@
 exports.handler = async (event, context) => {
+  if (event.httpMethod === "OPTIONS") {
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers":
+          "Origin, X-Requested-With, Content-Type, Accept"
+      },
+      body: JSON.stringify({ message: "Allowed." })
+    };
+  }
+
   if (event.httpMethod !== "POST") {
     return { statusCode: 405, body: "Method Not Allowed" };
   }
 
   const params = querystring.parse(event.body);
+  const result = params;
 
   return {
     statusCode: 200,
-    body: "Hello, World"
+    body: JSON.stringify(result)
   };
 };
